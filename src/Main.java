f0cabb3d7b97228c6708f7078eae2dc29e54f67b
SparkkkyZzx <miguelspark2007@gmail.com>
2026-04-03 14:14:02 -0400

    ATIVIDADE 6

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,12 @@
-//ATT 3
-
-
-import java.util.ArrayList;
-
-
-
-
 public class Main {
     public static void main(String[] args) {
 
-
-        ArrayList<carro> listaCarros = new ArrayList<>();
-        carro carro1 = new carro();
-        carro1.marca = "chevrolet";
-        carro1.modelo = "Chevetão";
-        carro1.ano = 1973;
+        carro carro1 = new carro("Chevrolet", "Chevetão", 1973);
 
 
         carro carro2 = new carro();
-        carro2.marca = "wolkswagen";
-        carro2.modelo = "Fuscão";
-        carro2.ano = 1938;
 
-
-        carro carro3 = new carro();
-        carro3.marca = "ford";
-        carro3.modelo = "Mustang";
-        carro3.ano = 2020;
-
-
-        listaCarros.add(carro1);
-        listaCarros.add(carro2);
-        listaCarros.add(carro3);
-
-
-        for (carro c : listaCarros) {
-            c.exibir();
-            System.out.println("-------------------");
-        }
+        carro1.exibir();
+        carro2.exibir();
     }
 }
--- src/carro.java
+++ src/carro.java
@@ -1,21 +1,35 @@
 public class carro {
-    String marca;
-    String modelo;
-    int ano;
 
+    private String marca;
+    private String modelo;
+    private int ano;
 
+    public carro(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
 
-
-    void exibir() {
-        System.out.println("Marca: " + marca);
-        System.out.println("Modelo: " + modelo);
-        System.out.println("Ano: " + ano);
+        if (ano < 1886 || ano > 2026) {
+            System.out.println("Ano inválido!");
+            this.ano = 0;
+        } else {
+            this.ano = ano;
+        }
     }
 
+    public carro() {
+        this.marca = "Desconhecido";
+        this.modelo = "Desconhecido";
+        this.ano = 0;
+    }
 
-    int idadeDoCarro() {
+    public int idadeDoCarro() {
         return 2026 - ano;
     }
 
-
+    public void exibir() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+        System.out.println("--------------------------");
+    }
 }
