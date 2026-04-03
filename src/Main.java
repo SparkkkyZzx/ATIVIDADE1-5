63170cbdaa6589481eef9b758a51467d5d8cd9ad
SparkkkyZzx <miguelspark2007@gmail.com>
2026-04-03 15:07:12 -0400

    ATIVIDADE9

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        new carro("Chevrolet", "Chevetão", 1973);
-        new carro("Ford", "Mustang", 1965);
+        carro c1 = new carro("Chevrolet", "Chevetão", 1973);
+        carro c2 = new carro("Volkswagen", "Fusca", 1980);
 
+        c1.exibir();
+        c2.exibir();
+
+        // método static
         carro.imprimirTotal();
+
+        // tentativa de alterar constante (vai dar erro)
+        // Carro.ANO_PRIMEIRO_CARRO = 1900;
+        // Não é possível alterar o valor porque a variável é final,
+        // ou seja, constante e não pode ser modificada após definida.
     }
 }
--- src/carro.java
+++ src/carro.java
@@ -4,12 +4,27 @@ public class carro {
     private String modelo;
     private int ano;
 
+    public static final int ANO_PRIMEIRO_CARRO = 1886;
+
+
     public static int totalCarros = 0;
+
+    public static void imprimirTotal() {
+        System.out.println("Total de carros: " + totalCarros);
+    }
+
+    public void setAno(int ano) {
+        if (ano < ANO_PRIMEIRO_CARRO || ano > 2026) {
+            System.out.println("Ano inválido!");
+        } else {
+            this.ano = ano;
+        }
+    }
 
     public carro(String marca, String modelo, int ano) {
         this.marca = marca;
         this.modelo = modelo;
-        this.ano = ano;
+        setAno(ano);
         totalCarros++;
     }
 
@@ -20,10 +35,14 @@ public class carro {
         totalCarros++;
     }
 
-    public static void imprimirTotal() {
-        System.out.println("Total de carros: " + totalCarros);
+    public void exibir() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+        System.out.println("--------------------------");
+    }
 
-        // tentativa de acessar atributo de instância
-        // ERRO!
+    public int idadeDoCarro() {
+        return 2026 - ano;
     }
 }
