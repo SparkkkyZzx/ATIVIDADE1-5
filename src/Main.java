facae19f47d9eba96066570ee23c9da44c814618
SparkkkyZzx <miguelspark2007@gmail.com>
2026-04-04 22:58:21 -0400

    atividdade 15

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Cliente c1 = new Cliente("João Silva", "123.456.789-00");
-        Cliente c2 = new Cliente("Maria Oliveira");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> nomes = new ArrayList<>();
+        int opcao = 0;
 
-        c1.exibir();
-        c2.exibir();
+        while (opcao != 3) {
+            System.out.println("\n1 - Cadastrar Nome");
+            System.out.println("2 - Listar Nomes");
+            System.out.println("3 - Sair");
+            System.out.print("Escolha: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            if (opcao == 1) {
+                System.out.print("Digite o nome: ");
+                String nome = scanner.nextLine();
+                nomes.add(nome);
+            } else if (opcao == 2) {
+                System.out.println("--- Lista de Nomes ---");
+                for (String n : nomes) {
+                    System.out.println(n);
+                }
+            }
+        }
+        scanner.close();
     }
+}
